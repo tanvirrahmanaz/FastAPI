bf03af7a62b0a57a22aa07a6c8f344e2df4fdf71
Tanvir Rahman <tanvirrahmanaz@gmail.com>
2026-06-29 23:17:24 +0600

    chcnagee

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException,Query
 import json
 
 app = FastAPI()
 
 def load_data():
-    with open('patients.json', 'r') as f:
+    with open('patients.json','r') as f:
         data = json.load(f)
-    return f
+    return data
 
 @app.get("/")
-def hello():
-    return {'message': 'hello world'}
+def home():
+    return {'message':"FastaAPI is running"}
 
-@app.get("/about")
-def about():
-    return {"message":"Tanvir Rahman learn FastAPI"}
+@app.get("/patients")
+def get_patients():
+    return {'message':"Patients Routs working"}
 
 @app.get('/view')
 def view():
-    data = load_data
+    data = load_data()
     return data
+
+@app.get('/patients/{patient_id}')
+def view_patient(patient_id:str):
+    #load all the patient data
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    # return {'error':'patient not found'}
+    raise HTTPException(status_code=404,detail="patient Not found")
+
+
+@app.get('/sort')
+def sort_patients(sort_by : str= Query(...,description='sort on the basis of height, weight or bmi'), order:str =Query('asc', description='sort in asc or desc order')):
+
+    valid_fields = ['height', 'weight','bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(status_code=400, detail=f"invalid field select from {valid_fields}")
